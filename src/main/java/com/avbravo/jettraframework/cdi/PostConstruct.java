1dfb1dc7a63abe0888c3c8df7a963e83b5b5ee3e
avbravo <avbravo@avbravo-IdeaPad-Gaming-3-15ARH7>
2025-02-05 22:24:38 -0500

    cdi

---
+++ src/main/java/com/avbravo/jettraframework/cdi/PostConstruct.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.avbravo.jettraframework.cdi;
+
+/**
+ *
+ * @author avbravo
+ */
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.METHOD})
+public @interface PostConstruct {
+}
